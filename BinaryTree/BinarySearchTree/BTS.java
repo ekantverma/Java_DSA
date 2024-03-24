47fed6d4ace21ed116945cc350819356875e8d7c
Ekant Verma <ekantvermasolanki@gmail.com>
2024-03-24 17:27:08 +0530

    Updated

--- BinaryTree/BinarySearchTree/BTS.java
+++ BinaryTree/BinarySearchTree/BTS.java
@@ -1,7 +1,10 @@
 package BinaryTree.BinarySearchTree;
 
+import java.util.*;
+
 public class BTS {
     public static void main(String[] args) {
         System.out.println("Hello");
+        Node root = new Node(1);
     }
 }
