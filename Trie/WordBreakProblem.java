823ef7b4cc320b1e0bfca3a4f00d95e4063f6df6
Ekant Verma <ekantvermasolanki@gmail.com>
2024-04-17 17:55:09 +0530

    updated

---
+++ Trie/WordBreakProblem.java
@@ -0,0 +1,76 @@
+package Trie;
+
+import java.util.*;
+
+public class WordBreakProblem {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        int level = 0;
+        int len = word.length();
+        int idx;
+
+        Node curr = root;
+
+        for (level = 0; level < len; level++) {
+            idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        int level;
+        int len = key.length();
+        int idx = 0;
+
+        Node curr = root;
+        for (level = 0; level < len; level++) {
+            idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+
+    }
+
+    public static boolean wordBreak(String key) {
+        if (key.length() == 0) {
+            return true;
+        }
+        for (int i = 1; i <= key.length(); i++) {
+            if (search(key.substring(0, i)) && wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+        String key = "i";
+        System.out.println(wordBreak(key));
+    }
+}
