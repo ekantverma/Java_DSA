23a075c7e8a06fdbb415fc5c041110fd118d0ddf
Ekant Verma <ekantvermasolanki@gmail.com>
2024-04-19 10:06:57 +0530

    updated

---
+++ Graphs/graph1.java
@@ -0,0 +1,51 @@
+package Graphs;
+
+import java.util.*;
+
+public class graph1 {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V]; // list of edges to be connected to the vertex
+        for (int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0 - Vertex
+        graph[0].add(new Edge(0, 1, 5));
+
+        // 1 - vertex
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        // 2 -vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 2));
+        graph[2].add(new Edge(2, 4, 4));
+
+        // 3 - vertex
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+
+        // 4 - vertex
+        graph[4].add(new Edge(4, 2, 2));
+
+        // 2nd neigbors
+        for (int i = 0; i < graph[2].size(); i++) {
+            Edge edge = graph[2].get(i);
+            System.out.println(edge.dest);
+        }
+    }
+}
