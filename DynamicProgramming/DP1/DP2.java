a24a659e776b98e8ca85fcc7d8f94d92de430ab1
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-26 16:55:57 +0530

    updated

--- DynamicProgramming/DP1/DP2.java
+++ DynamicProgramming/DP1/DP2.java
@@ -71,6 +71,23 @@ public class DP2 {
         return dp[n][W]; // We can print 2d matrix for more clarify
     }
 
+    public static boolean targetSumSubset(int arr[][], int sum) {
+        int n = arr.length;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                int v = arr[i-1];
+
+                if()
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
