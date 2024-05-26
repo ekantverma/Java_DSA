03ef2ab241176d4e2de4e2c2cf50e42937749764
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-26 19:43:41 +0530

    Questions added

--- DynamicProgramming/DP1/DP2.java
+++ DynamicProgramming/DP1/DP2.java
@@ -71,7 +71,7 @@ public class DP2 {
         return dp[n][W]; // We can print 2d matrix for more clarify
     }
 
-    public static boolean targetSumSubset(int arr[][], int sum) {
+    public static boolean targetSumSubset(int arr[], int sum) {
         int n = arr.length;
         boolean dp[][] = new boolean[n + 1][sum + 1];
 
@@ -81,26 +81,24 @@ public class DP2 {
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < sum + 1; j++) {
-                int v = arr[i-1];
+                int v = arr[i - 1];
 
-                if()
+                if (v <= j && dp[i - 1][j - v] == true) {
+                    dp[i][j] = true;
+                }
+
+                else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
             }
         }
+        return dp[n][sum];
     }
 
     public static void main(String[] args) {
-        int val[] = { 15, 14, 10, 45, 30 };
-        int wt[] = { 2, 5, 1, 3, 4 };
-        int W = 7;
-        int dp[][] = new int[val.length + 1][W + 1];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] = -1;
-            }
-        }
+        int[] arr = { 4, 2, 7, 1, 3 };
+        int sum = 6;
 
-        // System.out.println(knapsakeMemo(val, wt, W, val.length, dp));
-
-        System.out.println(knapsakeTab(val, wt, W));
+        System.out.println(targetSumSubset(arr, sum));
     }
 }
