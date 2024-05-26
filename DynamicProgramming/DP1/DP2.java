bd80ccc894e97251881b51228f52805dfda1a097
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-26 19:56:15 +0530

    Question added

--- DynamicProgramming/DP1/DP2.java
+++ DynamicProgramming/DP1/DP2.java
@@ -95,10 +95,35 @@ public class DP2 {
         return dp[n][sum];
     }
 
-    public static void main(String[] args) {
-        int[] arr = { 4, 2, 7, 1, 3 };
-        int sum = 6;
+    public static int unboundedKnapsake(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
 
-        System.out.println(targetSumSubset(arr, sum));
+        // dp initialization
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < W + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < W + 1; j++) {
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] val = { 15, 14, 10, 45, 30 };
+        int wt[] = { 2, 5, 1, 3, 4 };
+        int W = 7;
+
+        System.out.println(unboundedKnapsake(val, wt, W));
     }
 }
