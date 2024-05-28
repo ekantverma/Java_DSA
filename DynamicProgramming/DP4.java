5acb3e5ea077619da7c6d85cc103809773e87ebb
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-28 20:19:41 +0530

    Updated

--- DynamicProgramming/DP4.java
+++ DynamicProgramming/DP4.java
@@ -1,6 +1,30 @@
 public class DP4 {
     // We have to check operation in this strings and we have only two operation
     // add and delete
+    public static int stringConversion(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Initialization
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         String str1 = "pear";
