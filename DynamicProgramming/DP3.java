e0330d5768364a888980be3f6e3f564b3353ed7b
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-31 00:03:56 +0530

    updated

--- DynamicProgramming/DP3.java
+++ DynamicProgramming/DP3.java
@@ -45,11 +45,71 @@ public class DP3 {
         return dp[n][rodLength];
     }
 
-    public static void main(String[] args) {
-        int length[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int price[] = { 1, 5, 8, 9, 10, 17, 17, 20 };
-        int rodLength = 8;
+    public static int lcsRecursion(String str1, String str2, int n, int m) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
+            return 1 + lcsRecursion(str1, str2, n - 1, m - 1);
+        } else {
+            return Math.max(lcsRecursion(str1, str2, n - 1, m), lcsRecursion(str1, str2, n, m - 1));
+        }
+    }
 
-        System.out.println(rodCutting(length, price, rodLength));
+    public static int lcsMemoization(String str1, String str2, int n, int m, int dp[][]) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
+            return lcsMemoization(str1, str2, n - 1, m - 1, dp) + 1;
+        } else {
+            int ans1 = lcsMemoization(str1, str2, n - 1, m, dp);
+            int ans2 = lcsMemoization(str1, str2, n, m - 1, dp);
+            return Math.max(ans1, ans2);
+        }
+    }
+
+    public static int lcsTabulation(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdge";
+        String str2 = "abedg";
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(lcsTabulation(str1, str2));
     }
 }
