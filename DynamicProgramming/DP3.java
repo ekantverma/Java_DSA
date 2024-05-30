4087d4c30122e2156b9f9a972c0b4c0a71fad1f6
Ekant Verma <ekantvermasolanki@gmail.com>
2024-05-30 23:46:11 +0530

    Updated

--- DynamicProgramming/DP3.java
+++ DynamicProgramming/DP3.java
@@ -24,9 +24,32 @@ public class DP3 {
         return dp[n][sum];
     }
 
+    public static int rodCutting(int length[], int price[], int rodLength) {
+        int n = length.length;
+        int dp[][] = new int[n + 1][rodLength + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < rodLength + 1; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < rodLength + 1; j++) {
+                if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][rodLength];
+    }
+
     public static void main(String[] args) {
-        int coins[] = { 1, 2, 3 };
-        int sum = 3;
-        System.out.println(coinChange(coins, sum));
+        int length[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int price[] = { 1, 5, 8, 9, 10, 17, 17, 20 };
+        int rodLength = 8;
+
+        System.out.println(rodCutting(length, price, rodLength));
     }
 }
