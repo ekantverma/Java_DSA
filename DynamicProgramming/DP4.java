0ce7b9a2c75352d6ded364c91a0cde58eb796cb1
Ekant Verma <ekantvermasolanki@gmail.com>
2024-06-07 10:34:57 +0530

    Questions added

--- DynamicProgramming/DP4.java
+++ DynamicProgramming/DP4.java
@@ -26,9 +26,41 @@ public class DP4 {
         return dp[n][m];
     }
 
+    public static int longestCommonSubstring(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        // Table
+        int dp[][] = new int[n + 1][m + 1];
+        int ans = 0;
+
+        // Initialization
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = 0;
+        }
+        // bottom up
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int LIS(int arr[]) {
+
+    }
+
     public static void main(String[] args) {
-        String str1 = "pear";
-        String str2 = "sea";
-        System.out.println(stringConversion(str1, str2));
+        String str1 = "abcde";
+        String str2 = "abgce";
+        System.out.println(longestCommonSubstring(str1, str2));
     }
 }
