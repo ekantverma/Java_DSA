d39e985c364dff4d375f67fddd63d661ebca8acc
Ekant Verma <ekantvermasolanki@gmail.com>
2024-06-11 06:13:13 +0530

    Question added

---
+++ DynamicProgramming/DpAssignment.java
@@ -0,0 +1,32 @@
+public class DpAssignment {
+    public static int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    }
+
+    public static void tribonacciSequence(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        for (int i = 3; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(dp[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        tribonacciSequence(n);
+    }
+}
