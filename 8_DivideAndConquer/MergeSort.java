15387b2c7158c940544782221714a3995ad21885
Ekant Verma <ekantvermasolanki@gmail.com>
2024-06-23 20:06:06 +0530

    File Updated

--- 8_DivideAndConquer/MergeSort.java
+++ 8_DivideAndConquer/MergeSort.java
@@ -1,25 +1,25 @@
 public class MergeSort {
-    //sorting method (to do step1(divide) & step2(sort parts))
-    public static void mergeSort(int arr[], int si, int ei) { 
-        if(si >= ei) {
+    // sorting method (to do step1(divide) & step2(sort parts))
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
             return;
         }
-        int mid = si + (ei - si)/2; // or = (si + ei) / 2; 
+        int mid = si + (ei - si) / 2; // or = (si + ei) / 2;
         mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
+        mergeSort(arr, mid + 1, ei);
 
         merge(arr, si, mid, ei);
     }
 
-    //merge method to merge the sorted parts
+    // merge method to merge the sorted parts
     public static void merge(int arr[], int si, int mid, int ei) {
-        int temp[] = new int[ei-si+1];
-        int i = si; //idx for 1st sorted part
-        int j = mid+1; //idx for 2nd sorted part
-        int k = 0; //idx for temp;
+        int temp[] = new int[ei - si + 1];
+        int i = si; // idx for 1st sorted part
+        int j = mid + 1; // idx for 2nd sorted part
+        int k = 0; // idx for temp;
 
-        while(i <= mid && j <= ei) {
-            if(arr[i] < arr[j]) {
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
                 i++;
             } else {
@@ -29,31 +29,32 @@ public class MergeSort {
             k++;
         }
 
-        //for leftover elements of 1st sorted part
-        while(i <= mid) {
+        // for leftover elements of 1st sorted part
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
-        //for leftover elements of 2nd sorted part
-        while(j <= ei) {
+        // for leftover elements of 2nd sorted part
+        while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
-        //copy temp to original array
-        for(k=0, i=si; k<temp.length; k++, i++) {
+        // copy temp to original array
+        for (k = 0, i = si; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
     }
 
     public static void printArr(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i] +" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String args[]) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
-        mergeSort(arr, 0, arr.length-1);
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
         printArr(arr);
     }
 }
