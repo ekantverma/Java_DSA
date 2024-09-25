e83c666352e9cd307b323c2725d5282a98fc507b
Ekant Verma <ekantvermasolanki@gmail.com>
2024-09-25 19:17:32 +0530

    Updated

--- 00. Apna College Java-DSA/Divide and concure/Quick_sort_using_pivot.java
+++ 00. Apna College Java-DSA/Divide and concure/Quick_sort_using_pivot.java
@@ -1,50 +1,50 @@
 public class Quick_sort_using_pivot {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        int arr[] ={6,3,9,8,2,5};
-        quicksort(arr,0,arr.length-1);
+        int arr[] = { 6, 3, 9, 8, 2, 5 };
+        quicksort(arr, 0, arr.length - 1);
 
-        //printing arr
-        for(int i =0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
+        // printing arr
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
 
-    public static void quicksort(int arr[],int si, int ei){
+    public static void quicksort(int arr[], int si, int ei) {
 
-        //base case
-        if(si >= ei){
+        // base case
+        if (si >= ei) {
             return;
         }
 
-        //here maiking new function to get array according to pivot
-        int piIndex = piviot(arr,si,ei);
-        //for sorting the left part
-        quicksort(arr,si,piIndex-1);
+        // here maiking new function to get array according to pivot
+        int piIndex = piviot(arr, si, ei);
+        // for sorting the left part
+        quicksort(arr, si, piIndex - 1);
 
-        //for sorting the rigth part
-        quicksort(arr,piIndex+1,ei);
-
+        // for sorting the rigth part
+        quicksort(arr, piIndex + 1, ei);
 
     }
 
-    public static int piviot(int arr[],int si, int ei){
+    public static int piviot(int arr[], int si, int ei) {
 
-        //this is piviot around which we will sort the elements
+        // this is piviot around which we will sort the elements
         int piviot = arr[ei];
-        
-        //int i for difining the positions of the swapped elements
-        //taking si-1 because it will not have any position before finding a element below piviot element
 
-        int i = si-1;
-        
-        for(int j = si; j< ei; j++){
-            if(arr[j]<piviot){
-                
+        // int i for difining the positions of the swapped elements
+        // taking si-1 because it will not have any position before finding a element
+        // below piviot element
+
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] < piviot) {
+
                 i++;
-                //swap
+                // swap
 
                 int temp = arr[j];
                 arr[j] = arr[i];
@@ -52,14 +52,15 @@ public class Quick_sort_using_pivot {
             }
         }
 
-        //for putting the piviot in right place
+        // for putting the piviot in right place
 
         i++;
         int temp = piviot;
         arr[ei] = arr[i];
         arr[i] = temp;
-        //here i is the position of piviot
+
+        // here i is the position of piviot
         return i;
     }
-    
+
 }
